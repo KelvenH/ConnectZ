621977730354186d6461ebf647218706004bec0b
KelvenH <kelvenhughes@me.com>
2022-05-28 13:05:29 +0000

    Check for correct number of arguments provided

---
+++ README.md
@@ -0,0 +1,2 @@
+## Acknowledgements:
+    Pass CLI Arguments https://www.youtube.com/watch?v=Y4A_0tCe8ik
--- connectz.py
+++ connectz.py
@@ -1,5 +1,18 @@
-def main():
-    print("Initial set-up")
+# sys enables use of CLI arguments
+import sys
+print(sys.argv)
+
+
+def main() -> None:
+    check_args()
+
+def check_args():
+    """ Check correct number of arguments provided in CLI  """
+    if len(sys.argv) != 2:
+        print("connectz.py: Provide one input file")
+        exit()
+    else:
+        print("args check passed")
 
 
 if __name__ == '__main__':
