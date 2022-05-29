00b776e79d80cc59b211098d605f58c1b276ace7
KelvenH <kelvenhughes@me.com>
2022-05-29 18:23:58 +0000

    Function to check for row win and validate last move

--- connectz.py
+++ connectz.py
@@ -7,22 +7,22 @@ def main() -> None:
     
     file_name = check_args()                                        # validate CLI inputs and return file name
     game_inputs = read_file(file_name)                              # validate file content and return game data
-    game_grid, cols, rows, target = build_game(game_inputs)         # check for invalid games & populate 'grid ref' view of game
+    game_grid, cols, rows, target, last_move_id = build_game(game_inputs) # check for invalid games & populate 'grid ref' view of game
     player_A_moves, player_B_moves = create_player_moves(game_grid) # create seperate player moves
     # check results & determine win/draw outcomes
-    check_row_win(player_A_moves, player_B_moves, rows)                    
-    # clear lists for next game                                     # clear lists to ensure not impairing subsequent runs
-
+    check_row_win(player_A_moves, player_B_moves, rows, target, last_move_id)                    
+   
 
 def check_args():
     """ Check number of arguments provided in CLI  """
     if len(sys.argv) != 2:
         print("connectz.py: Provide one input file")   # Incorrect number of arguments provided
         exit()
+    
     else:
         """ Return filename if check passed """
         file_name = sys.argv[1]
-        # print(f"args check passed filename = {file_name}")        
+       
         return file_name
 
 
@@ -38,8 +38,8 @@ def read_file(file_name):
             exit()
 
         valid_inputs = validate_content(game_file)
-        
         game_file.close()
+
         return valid_inputs
 
     except FileNotFoundError:
@@ -53,8 +53,14 @@ def validate_content(game_file):
 
     # Check game initialser row
     setup_line = content.split('\n', 1)[0]  # read 1st line of file
-    setup_values = setup_line.split()       # convert space-separated string to a list
     
+    try:
+        setup_values = [int(num) for num in setup_line.split()]       # convert space-separated string to a list of ints
+    
+    except ValueError:
+        print(8) # Non-digit value encountered (in set-up line)
+        exit()
+
     # Invalid number of dimension values in file to initialize game
     if len(setup_values) != 3:
         print(8)
@@ -65,7 +71,7 @@ def validate_content(game_file):
         file_values = [int(value) for value in content.split()]  
     
     except ValueError:
-        print(8) # Non-digit value encountered
+        print(8) # Non-digit value encountered (in game moves)
         exit()
     
     # Check for valid integers
@@ -87,7 +93,7 @@ def build_game(game_inputs):
     cols = game_inputs[0]
     rows = game_inputs[1]
     target = game_inputs[2]
-    del game_inputs[0:3]        # remove game dimension values retaining only player moves
+    del game_inputs[0:3]    # remove game dimension values retaining only player moves
     
     game_grid = []
     col_tracker = []              
@@ -123,36 +129,68 @@ def build_game(game_inputs):
         turn_id = player_id + turn_col_id + turn_row_id
         game_grid.append(turn_id)
     
-    print(game_grid)
-    return (game_grid, cols, rows, target)
+        # Identify last move of game
+        last_move_id = game_grid[-1]
+
+    return (game_grid, cols, rows, target, last_move_id)
 
 
 def create_player_moves(game_grid):
-    print(game_grid)
+    """ Split gamegrid into seperate player moves"""
     
     # extract player A moves
     player_A = "A"
     player_A_moves = [move for move in game_grid if player_A in move]
-    print(player_A_moves)
 
     # extract player B moves
     player_B = "B"
     player_B_moves = [move for move in game_grid if player_B in move]
-    print(player_B_moves)
 
     return (player_A_moves, player_B_moves)
 
 
-def check_row_win(player_A_moves, player_B_moves, rows):
-    print(player_A_moves)
-    print(player_B_moves)
+def check_row_win(player_A_moves, player_B_moves, rows, target, last_move_id):
+    """ Check for winning rows within player moves"""
     max_rows = rows
-    row_counter = 1
+
+    # Check player A rows
+    row_counter_A = 1
     for x in range(max_rows):
-        print(row_counter)
-        row_counter += 1
-        #search_term = "Y" + str(row_counter) 
-        #print(search_term)
+        search_term = "Y" + str(row_counter_A)
+        row_slice_A = [id for id in player_A_moves if search_term in id]
+        row_counter_A += 1
+        if len(row_slice_A) >= target:
+            player="A"
+            last_move = row_slice_A[-1]
+            check_last_move(player, last_move, last_move_id)
+
+    # Check player B rows
+    row_counter_B = 1
+    for x in range(max_rows):
+        search_term = "Y" + str(row_counter_B)
+        row_slice_B = [id for id in player_B_moves if search_term in id]
+        row_counter_B += 1
+        if len(row_slice_B) >= target:
+            player="B"
+            last_move = row_slice_B[-1]
+            check_last_move(player, last_move, last_move_id)
+
+
+def check_last_move(player, last_move, last_move_id):
+    """ Check players last move from wining line against last turn played """
+    if last_move == last_move_id:
+
+        if player == "A":
+            print(1) # Player 1 (A) Win
+            exit()
+
+        if player == "B":
+            print(2)  # Player 2 (B) Win
+            exit()
+
+    else:
+        print(4) # Illegal continue
+        exit()
 
 
 if __name__ == '__main__':
